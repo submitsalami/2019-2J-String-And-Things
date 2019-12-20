9ff8153ea65db80b6c656e0e88f1884c7a09a24c
Nyein1197 <44962355+Nyein1197@users.noreply.github.com>
2019-12-20 15:12:04 +0900

    Nyein Nyein

---
+++ CH9/EX2.java
@@ -0,0 +1,23 @@
+
+/**
+ * クラス EX2 の注釈をここに書きます.
+ * 
+ * @author (Nyein)
+ * @version (2019/11/1)
+ */
+import java.util.Arrays;
+public class EX2{
+    public static void main(String[]args){
+       String n = "nn";
+       System.out.print(Arrays.toString(letterHist(n)));
+    }
+    //Our first subI problem
+    //1.Write a method call letterHiat
+    //2.takes a string as a parameter
+    //3.returns a historgram(array)
+    public static int[]letterHist(String n){
+       int[]a = new int[26];
+       
+       return a;
+    }
+}
