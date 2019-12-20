cb38aa8cd874190021d0751a30e1ddcbc88a19eb
Pan Zun <44962361+zun21@users.noreply.github.com>
2019-12-20 15:13:12 +0900

    Pan Zun

---
+++ Chapter9/Ex2.java
@@ -0,0 +1,23 @@
+
+/**
+ * Write a description of class Ex2 here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+import java.util.Arrays;
+public class Ex2
+{
+    public static void main(String[]args){
+        String p = "zz";
+        System.out.print(Arrays.toString(letterHist(p)));
+    }
+    //Create a method called letterHist
+    //take a String as a parameter
+    //returns a histogram(array)
+    public static int []letterHist(String p){
+        int[]a = new int[26];
+        return a;
+       
+    }
+}
